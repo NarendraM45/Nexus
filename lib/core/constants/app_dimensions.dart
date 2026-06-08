41f6661e9c7a5c9c0f3b3c964d3b0a9a3fc43213
Narendra <narendra.pro1212@gmail.com>
2026-06-08 11:25:59 +0530

    chore: finalize core constant definitions

---
+++ lib/core/constants/app_dimensions.dart
@@ -0,0 +1,10 @@
+class AppDimensions {
+  static const double paddingS = 8;
+  static const double paddingM = 16;
+  static const double paddingL = 24;
+  static const double paddingXL = 32;
+  static const double radiusS = 12;
+  static const double radiusM = 20;
+  static const double radiusL = 28;
+  static const double cardElevation = 0; // using shadows manually
+}
---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,4 @@
+class AppStrings {
+  static const String appName = 'Nexus';
+  static const String tagline = 'The Hub for Doers.';
+}
